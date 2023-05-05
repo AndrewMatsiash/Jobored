1f69aa71d172f1c4d1e15af234fab9e3c7c9475a
AndrewMatsiash <rabotatnadomne@gmail.com>
2023-05-05 12:58:23 +0300

    feat:implement add and remove vacancy to LocalStorage

--- my-app/src/pages/SearchPage/ components/JobCard/JobCard.tsx
+++ my-app/src/pages/SearchPage/ components/JobCard/JobCard.tsx
@@ -1,15 +1,60 @@
 import './JobCard.css';
 import { Card, Flex, Stack, Text } from '@mantine/core';
 import { Link } from 'react-router-dom';
-import { IconMapPin } from '@tabler/icons-react';
+import { IconMapPin, IconStar } from '@tabler/icons-react';
 import { IVacancy } from '../../../../types/vacancy';
+import React from 'react';
 
 export const JobCard = ({ vacancy }: { vacancy: IVacancy }) => {
-  console.log(vacancy);
+  const [isSelect, setIsSelect] = React.useState(false);
+
+  const addVacancyToFavorites = (vacancy: IVacancy) => {
+    const data = localStorage.getItem('favorites');
+    if (!data) {
+      localStorage.setItem('favorites', JSON.stringify([vacancy]));
+    } else {
+      const favorites = JSON.parse(data);
+      localStorage.setItem(
+        'favorites',
+        JSON.stringify([...favorites, vacancy])
+      );
+    }
+  };
+
+  const removeVacancyFromFavorites = (vacancyId: number) => {
+    const data = localStorage.getItem('favorites');
+    if (data) {
+      const favorites = JSON.parse(data).filter(
+        (f: IVacancy) => f.id !== vacancyId
+      );
+      localStorage.setItem('favorites', JSON.stringify(favorites));
+    }
+  };
+
+  const toggleSelect = () => {
+    setIsSelect(prevIsSelect => !prevIsSelect);
+  };
+
+  const handlerClick = () => {
+    toggleSelect();
+    if (!isSelect) {
+      addVacancyToFavorites(vacancy);
+    } else {
+      removeVacancyFromFavorites(vacancy.id);
+    }
+  };
 
   return (
     <Card shadow="sm" padding={'xl'}>
       <Stack spacing={'sm'}>
+        <Flex justify={'end'}>
+          <IconStar
+            onClick={() => {
+              handlerClick();
+            }}
+            color={isSelect ? 'green' : 'black'}
+          />
+        </Flex>
         <Text size="xl" weight={600}>
           <Link to={`/job/${vacancy.id}`}>{vacancy.profession}</Link>
         </Text>
