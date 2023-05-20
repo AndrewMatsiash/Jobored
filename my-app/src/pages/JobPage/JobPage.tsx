c5a26bd3f17982c5d6234c5c877b43a5efe8e2a4
AndrewMatsiash <rabotatnadomne@gmail.com>
2023-05-20 17:10:53 +0300

    fix:fix error build

--- my-app/src/pages/JobPage/JobPage.tsx
+++ my-app/src/pages/JobPage/JobPage.tsx
@@ -1,7 +1,7 @@
 import { useParams } from 'react-router-dom';
 import JobCard from '../SearchPage/ components/JobCard';
 import { useFetchDataVacancy } from '../../hooks/useFetchDataVacancy';
-import { Center, Container, Loader, Space, Stack } from '@mantine/core';
+import { Center, Container, Loader, Stack } from '@mantine/core';
 
 export const JobPage = () => {
   const { id } = useParams();
